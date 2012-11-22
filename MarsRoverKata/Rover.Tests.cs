317717dbac2aa9583ff9595e0dd9efb0c414ae24
Martin Salias <martin at salias dot com dot ar>
2012-11-21 22:29:33 -0300

    Initial Commit

---
+++ MarsRoverKata/Rover.Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using MarsRover;
+
+namespace MarsRover.Tests
+{
+    [TestFixture]
+    class BasicMoves
+    {
+        [TestCase]
+        public void InitialPosition() {
+            var rover = new Rover(2, 4);
+
+            Assert.AreEqual(2, rover.X);
+            Assert.AreEqual(4, rover.Y);
+        }
+    }
+}
---
+++ MarsRoverKata/Rover.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+    public class Rover
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Rover(int initX, int initY)
+        {
+            X = initX;
+            Y = initY;
+        }
+
+
+    }
+}
